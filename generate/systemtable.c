71f3685a856c40873dbd2d2453d8abe92f01ee9e
Ronald G. Minnich <rminnich@gmail.com>
2021-02-09 07:43:49 -0800

    Add systemtable and hooks for building UEFI "things"
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ generate/systemtable.c
@@ -0,0 +1,9 @@
+#include <efi.h>
+#include <efilib.h>
+
+// Application entrypoint (must be set to 'efi_main' for gnu-efi crt0 compatibility)
+int main(int argc, char *argv[])
+{
+	EFI_SYSTEM_TABLE s;
+	
+}
