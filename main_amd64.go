7e991016e905bc35e5103f2dfecd844217185972
Ronald G. Minnich <rminnich@gmail.com>
2021-02-23 10:13:42 -0800

    segv: adjust Rip after calling Dispatch
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- main_amd64.go
+++ main_amd64.go
@@ -17,13 +17,13 @@ import (
 
 func segv(p *ptrace.Tracee, i *unix.SignalfdSiginfo, inst *x86asm.Inst, r *syscall.PtraceRegs) error {
 	addr := uintptr(i.Addr)
-	//	pc := r.Rip
+	pc := r.Rip
 	log.Printf("================={SEGV START FUNCTION @ %#x", addr)
-	defer log.Printf("===========} done SEGV @ %#x", addr)
 	if err := services.Dispatch(&services.Fault{Proc: p, Info: i, Inst: inst, Regs: r}); err != nil {
 		return fmt.Errorf("Don't know what to do with %v: %v", ptrace.CallInfo(i, inst, r), err)
 	}
 	// Advance to the next instruction. This advance should only happen if the dispatch worked?
 	r.Rip += uint64(inst.Len)
+	defer log.Printf("===========} done SEGV @ %#x, rip was %#x, advance to %#x", addr, pc, r.Rip)
 	return nil
 }
