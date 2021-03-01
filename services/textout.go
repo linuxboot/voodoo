9041cce6ba8acb41e6164b932580b921c38281a4
Ronald G. Minnich <rminnich@gmail.com>
2021-02-28 20:32:32 -0800

    Try to get text output back.
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- services/textout.go
+++ services/textout.go
@@ -3,6 +3,7 @@ package services
 import (
 	"log"
 
+	"github.com/linuxboot/voodoo/ptrace"
 	"github.com/linuxboot/voodoo/table"
 	"github.com/linuxboot/voodoo/uefi"
 )
@@ -31,9 +32,24 @@ func (t *TextOut) Base() ServBase {
 // Call implements service.Call
 func (t *TextOut) Call(f *Fault, op Func) error {
 	log.Printf("TextOut services: %v(%#x), arg type %T, args %v", table.SimpleTextOutServicesNames[uint64(op)], op, f.Inst.Args, f.Inst.Args)
+	f.Regs.Rax = uefi.EFI_SUCCESS
 	switch op {
+	case table.STOutOutputString:
+		args := ptrace.Args(f.Proc, f.Regs, 6)
+		log.Printf("StOutOutputString args %#x", args)
+		n, err := f.Proc.ReadStupidString(uintptr(args[1]))
+		if err != nil {
+			return err
+		}
+		log.Printf("stupid is %q", n)
+		if err := f.Proc.SetRegs(f.Regs); err != nil {
+			return err
+		}
+		return nil
+	case table.STOutSetAttribute:
+		log.Printf("Fuck STOutSetAttribute")
 	case table.STOutMode:
-		panic("stoutmode")
+		log.Printf("FUCK table.STOutMode")
 	default:
 		panic("unsup textout")
 		f.Regs.Rax = uefi.EFI_UNSUPPORTED
