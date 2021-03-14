cb79fd22e3fbe5ec2baa49f1c122d37a82ae58b8
Ronald G. Minnich <rminnich@gmail.com>
2021-03-14 09:14:04 -0700

    First step on the way to kvm
    Some booters run at addresses below 0x200000
    
    Linux hits them with an immediate segv.
    
    So ... we're screwed
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ kvm/kvm.go
@@ -0,0 +1,384 @@
+// Package ptrace provides an interface to the ptrace system call.
+package ptrace
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"os"
+	"runtime"
+	"syscall"
+
+	"golang.org/x/sys/unix"
+)
+
+var (
+	// ErrTraceeExited is returned when a command is executed on a tracee
+	// that has already exited.
+	ErrTraceeExited = errors.New("tracee exited")
+	// Debug can be set externally to trace activity.
+	Debug = func(string, ...interface{}) {}
+)
+
+// An Event is sent on a Tracee's event channel whenever it changes state.
+type Event interface{}
+
+// A Tracee is a process that is being traced.
+type Tracee struct {
+	proc   *os.Process
+	events chan Event
+	err    chan error
+	cmds   chan func()
+}
+
+// PID returns the PID for a Tracee.
+func (t *Tracee) PID() int { return t.proc.Pid }
+
+// Events returns the events channel for the tracee.
+func (t *Tracee) Events() <-chan Event {
+	return t.events
+}
+
+// Exec executes a process with tracing enabled, returning the Tracee
+// or an error if an error occurs while executing the process.
+func Exec(name string, argv []string) (*Tracee, error) {
+	t := &Tracee{
+		events: make(chan Event, 1),
+		err:    make(chan error, 1),
+		cmds:   make(chan func()),
+	}
+
+	err := make(chan error)
+	proc := make(chan *os.Process)
+	go func() {
+		runtime.LockOSThread()
+		p, e := os.StartProcess(name, argv, &os.ProcAttr{
+			Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
+			Sys: &syscall.SysProcAttr{
+				Ptrace:    true,
+				Pdeathsig: syscall.SIGCHLD,
+			},
+		})
+		proc <- p
+		err <- e
+		if e != nil {
+			return
+		}
+		go t.wait()
+		t.trace()
+	}()
+	t.proc = <-proc
+	return t, <-err
+}
+
+// Attach attaches to the given process.
+func Attach(pid int) (*Tracee, error) {
+	t := &Tracee{
+		events: make(chan Event, 1),
+		err:    make(chan error, 1),
+		cmds:   make(chan func()),
+	}
+
+	err := make(chan error, 1)
+	proc := make(chan *os.Process)
+	go func() {
+		runtime.LockOSThread()
+		err <- syscall.PtraceAttach(pid)
+		p, e := os.FindProcess(pid)
+		proc <- p
+		err <- e
+		if e != nil {
+			return
+		}
+		go t.wait()
+		t.trace()
+	}()
+	t.proc = <-proc
+	return t, <-err
+}
+
+// Detach detaches the tracee, allowing it to continue its execution normally.
+// No more tracing is performed, and no events are sent on the event channel
+// until the tracee exits.
+func (t *Tracee) Detach() error {
+	err := make(chan error, 1)
+	if t.do(func() { err <- syscall.PtraceDetach(t.proc.Pid) }) {
+		return <-err
+	}
+	return ErrTraceeExited
+}
+
+// SingleStep continues the tracee for one instruction.
+func (t *Tracee) SingleStep() error {
+	err := make(chan error, 1)
+	if t.do(func() { err <- syscall.PtraceSingleStep(t.proc.Pid) }) {
+		return <-err
+	}
+	return ErrTraceeExited
+}
+
+// Continue makes the tracee execute unmanaged by the tracer.  Most commands are not
+// possible in this state, with the notable exception of sending a
+// syscall.SIGSTOP signal.
+func (t *Tracee) Continue() error {
+	err := make(chan error, 1)
+	sig := 0
+	if t.do(func() { err <- syscall.PtraceCont(t.proc.Pid, sig) }) {
+		return <-err
+	}
+	return ErrTraceeExited
+}
+
+// Syscall runs the inferior until it hits, or returns from, a system call.
+func (t *Tracee) Syscall() error {
+	if t.cmds == nil {
+		return ErrTraceeExited
+	}
+	errchan := make(chan error, 1)
+	t.cmds <- func() {
+		err := syscall.PtraceSyscall(t.proc.Pid, 0)
+		errchan <- err
+	}
+	return <-errchan
+}
+
+// SendSignal sends the given signal to the tracee.
+func (t *Tracee) SendSignal(sig syscall.Signal) error {
+	err := make(chan error, 1)
+	if t.do(func() { err <- syscall.Kill(t.proc.Pid, sig) }) {
+		return <-err
+	}
+	return ErrTraceeExited
+}
+
+// grabs a word at the given address.
+func peek(pid int, address uintptr) (uint64, error) {
+	word := make([]byte, 8 /* 8 should really be sizeof(uintptr)... */)
+	nbytes, err := syscall.PtracePeekData(pid, address, word)
+	if err != nil || nbytes != 8 /*sizeof(uintptr)*/ {
+		return 0, err
+	}
+	v := uint64(0x2Bc0ffee)
+	err = binary.Read(bytes.NewReader(word), binary.LittleEndian, &v)
+	return v, err
+}
+
+// ReadWord reads the given word from the inferior's address space.
+func (t *Tracee) ReadWord(address uintptr) (uint64, error) {
+	err := make(chan error, 1)
+	value := make(chan uint64, 1)
+	if t.do(func() {
+		v, e := peek(t.proc.Pid, address)
+		value <- v
+		err <- e
+	}) {
+		return <-value, <-err
+	}
+	return 0, errors.New("ReadWord: Unreachable")
+}
+
+// grabs a word at the given address.
+func poke(pid int, address uintptr, word uint64) error {
+	/* convert the word into the byte array that PtracePokeData needs. */
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.LittleEndian, word)
+	if err != nil {
+		return err
+	}
+
+	nbytes, err := syscall.PtracePokeData(pid, address, buf.Bytes())
+	if err != nil || nbytes != 8 /*sizeof(uint64)*/ {
+		return err
+	}
+	return nil
+}
+
+// WriteWord writes the given word into the inferior's address space.
+func (t *Tracee) WriteWord(address uintptr, word uint64) error {
+	err := make(chan error, 1)
+	if t.do(func() { err <- poke(t.proc.Pid, address, word) }) {
+		return <-err
+	}
+	return ErrTraceeExited
+}
+
+func (t *Tracee) Write(address uintptr, data []byte) error {
+	err := make(chan error, 1)
+	Debug("Write %#x %#x", address, data)
+	if t.do(func() {
+		_, e := syscall.PtracePokeData(t.proc.Pid, address, data)
+		err <- e
+	}) {
+		return <-err
+	}
+	return ErrTraceeExited
+}
+
+// Read grabs memory starting at the given address, for len(data) bytes.
+func (t *Tracee) Read(address uintptr, data []byte) error {
+	err := make(chan error, 1)
+	if t.do(func() {
+		_, e := syscall.PtracePeekData(t.proc.Pid, address, data)
+		err <- e
+	}) {
+		return <-err
+	}
+	return ErrTraceeExited
+}
+
+// ReadStupidString reads a UEFI-style string, i.e. one composed of words, not bytes.
+// We're gonna party like it's 1899.
+func (t *Tracee) ReadStupidString(address uintptr) (string, error) {
+	var s string
+	var w [2]byte
+	for {
+		if err := t.Read(address, w[:]); err != nil {
+			return "", err
+		}
+		if w[0] == 0 && w[1] == 0 {
+			break
+		}
+		s = s + string(w[:1])
+		address += 2
+	}
+	return s, nil
+}
+
+// GetRegs reads the registers from the inferior.
+func (t *Tracee) GetRegs() (*syscall.PtraceRegs, error) {
+	errchan := make(chan error, 1)
+	value := make(chan *syscall.PtraceRegs, 1)
+	if t.do(func() {
+		var regs syscall.PtraceRegs
+		err := syscall.PtraceGetRegs(t.proc.Pid, &regs)
+		value <- &regs
+		errchan <- err
+	}) {
+		return <-value, <-errchan
+	}
+	return &syscall.PtraceRegs{}, errors.New("GetRegs: Unreachable")
+}
+
+// GetIPtr reads the instruction pointer from the inferior and returns it.
+func (t *Tracee) GetIPtr() (uintptr, error) {
+	errchan := make(chan error, 1)
+	value := make(chan uintptr, 1)
+	if t.do(func() {
+		var regs syscall.PtraceRegs
+		regs.Rip = 0
+		err := syscall.PtraceGetRegs(t.proc.Pid, &regs)
+		value <- uintptr(regs.Rip)
+		errchan <- err
+	}) {
+		return <-value, <-errchan
+	}
+	return 0, ErrTraceeExited
+}
+
+// SetIPtr sets the instruction pointer for a Tracee.
+func (t *Tracee) SetIPtr(addr uintptr) error {
+	errchan := make(chan error, 1)
+	if t.do(func() {
+		var regs syscall.PtraceRegs
+		err := syscall.PtraceGetRegs(t.proc.Pid, &regs)
+		if err != nil {
+			errchan <- err
+			return
+		}
+		regs.Rip = uint64(addr)
+		err = syscall.PtraceSetRegs(t.proc.Pid, &regs)
+		errchan <- err
+	}) {
+		return <-errchan
+	}
+	return ErrTraceeExited
+}
+
+// SetRegs sets regs for a Tracee.
+func (t *Tracee) SetRegs(regs *syscall.PtraceRegs) error {
+	errchan := make(chan error, 1)
+	if t.do(func() {
+		err := syscall.PtraceSetRegs(t.proc.Pid, regs)
+		errchan <- err
+	}) {
+		return <-errchan
+	}
+	return ErrTraceeExited
+}
+
+// GetSiginfo reads the signal information for the signal that stopped the inferior.  Only
+// valid on Unix if the inferior is stopped due to a signal.
+func (t *Tracee) GetSiginfo() (*unix.SignalfdSiginfo, error) {
+	errchan := make(chan error, 1)
+	value := make(chan *unix.SignalfdSiginfo, 1)
+	if t.do(func() {
+		si, err := GetSigInfo(t.proc.Pid)
+		errchan <- err
+		value <- si
+	}) {
+		return <-value, <-errchan
+	}
+	return nil, ErrTraceeExited
+}
+
+// ClearSignal clears the last signal the inferior received.
+// This could allow the inferior
+// to continue after a segfault, for example.
+func (t *Tracee) ClearSignal() error {
+	errchan := make(chan error, 1)
+	if t.do(func() {
+		errchan <- ClearSignals(int(t.proc.Pid))
+	}) {
+		return <-errchan
+	}
+	return ErrTraceeExited
+}
+
+// Sends the command to the tracer go routine.	Returns whether the command
+// was sent or not. The command may not have been sent if the tracee exited.
+func (t *Tracee) do(f func()) bool {
+	if t.cmds != nil {
+		t.cmds <- f
+		return true
+	}
+	return false
+}
+
+// Close closes a Tracee.
+func (t *Tracee) Close() error {
+	var err error
+	select {
+	case err = <-t.err:
+	default:
+		err = nil
+	}
+	close(t.cmds)
+	t.cmds = nil
+
+	syscall.Kill(t.proc.Pid, syscall.SIGKILL)
+	return err
+}
+
+func (t *Tracee) wait() {
+	defer close(t.err)
+	for {
+		state, err := t.proc.Wait()
+		if err != nil {
+			t.err <- err
+			close(t.events)
+			return
+		}
+		if state.Exited() {
+			t.events <- Event(state.Sys().(syscall.WaitStatus))
+			close(t.events)
+			return
+		}
+		t.events <- Event(state.Sys().(syscall.WaitStatus))
+	}
+}
+
+func (t *Tracee) trace() {
+	for cmd := range t.cmds {
+		cmd()
+	}
+}
