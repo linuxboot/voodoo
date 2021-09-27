295ae5ae4435143591187eabff96cf61eb65572a
Ronald G. Minnich <rminnich@gmail.com>
2021-09-27 08:03:34 -0700

    checkpoint. Closer.
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- efi_selftest/include/common.h
+++ efi_selftest/include/common.h
@@ -1,4 +1,6 @@
 #ifndef COMMON_H
 #define COMMON_H
+#define NULL ((void *)0)
+#define CONFIG_IS_ENABLED(x) 1
 #include <linux/types.h>
 #endif
--- efi_selftest/include/linux/types.h
+++ efi_selftest/include/linux/types.h
@@ -1,16 +1,19 @@
 #ifndef LINUX_TYPES_H
-#define LINUX_TYPE_H
+#define LINUX_TYPES_H
 // fuck all this include shit, we know what they are.
 typedef unsigned char u8;
 typedef unsigned char uint8_t;
 typedef unsigned short u16;
 typedef unsigned short uint16_t;
 typedef unsigned long u32;
-//typedef unsigned long uint32_t;
+typedef unsigned long uint32_t;
 typedef unsigned long long ulong;
 typedef unsigned long long u64;
 typedef unsigned long long __u64;
 typedef unsigned long long uint64_t;
+
+typedef unsigned long long size_t;
+typedef long long ssize_t;
 
 typedef char s8;
 typedef short s16;
@@ -26,10 +29,23 @@ typedef u16 __le16;
 typedef u32 __le32;
 typedef u64 __le64;
 
+typedef struct jmp_buf_data {
+	uint8_t data[32];
+} jmp_buf_data;
+
 #define __packed	__attribute__((packed))
 #define __aligned(x)		__attribute__((aligned(x)))
 #define aligned_u64 __u64 __aligned(8)
-#define CONFIG_IS_ENABLED(x) 0
+
 
 typedef unsigned long long loff_t;
+
+struct blk_desc {
+	int whatevs;
+};
+
+struct udevice {
+	u8 nm[256];
+};
+
 #endif 
