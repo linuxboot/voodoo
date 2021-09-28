f78ccdb8d16bf64e95f1ee0ae732f2ee714cdc9a
Ronald G. Minnich <rminnich@gmail.com>
2021-09-28 09:19:56 -0700

    checkpoint commit
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- efi_selftest/include/common.h
+++ efi_selftest/include/common.h
@@ -1,6 +1,16 @@
 #ifndef COMMON_H
 #define COMMON_H
 #define NULL ((void *)0)
+#define true (1)
+#define false (0)
 #define CONFIG_IS_ENABLED(x) 1
+#define ARP_HLEN (6)
 #include <linux/types.h>
+#include <string.h>
+int printf(const char *format, ...);
+
+
+// just give it a fucking rest, will you?
+
+
 #endif
